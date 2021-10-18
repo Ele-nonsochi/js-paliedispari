83b2ae743539a0473968ee5420880bd28dfce1e3
Ele-nonsochi <elea.luzi25@gmail.com>
2021-10-18 16:21:55 +0200

    Inizio esercizio

---
+++ pari e dispari/scriptped.js
@@ -0,0 +1,24 @@
+/*
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+const numeroUtente = prompt("Inserire un numero pari o dispari")
+const betweenNumber = prompt("Inserire un numero da 1 a 5")
+
+
+
+
+
+
+
+function () {
+    if (number % 2 === 0) {
+        return 'even'
+    } else {
+        return 'odd'
+    }
+}
