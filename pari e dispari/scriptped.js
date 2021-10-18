d1a377e29639acc29761db71e8e7812d829759fb
Ele-nonsochi <elea.luzi25@gmail.com>
2021-10-18 17:26:12 +0200

    Esercizio completato

--- pari e dispari/scriptped.js
+++ pari e dispari/scriptped.js
@@ -6,19 +6,51 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
-const numeroUtente = prompt("Inserire un numero pari o dispari")
-const betweenNumber = prompt("Inserire un numero da 1 a 5")
+//Inserisco i numeri 
 
+const numeroPariDispari = prompt("Inserire un numero pari o dispari")
+const numeroUtente = prompt("Inserire un numero da 1 a 5")
 
+//Creo un numero casuale
 
+function generateRandomNumber(minNum = 1, maxNum = 10) {
+    const randomNum = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+    return randomNum
+}
 
+//Constanti per calcolare i miei numeri 
 
+const numeroGenerato = generateRandomNumber(1, 5)
+const totale = numeroUtente + numeroGenerato
 
+//Variabile che mi ritorna se il numero è pari o dispari 
 
-function () {
+const risultato = pariDispari(totale)
+
+function pariDispari(number) {
     if (number % 2 === 0) {
-        return 'even'
+        return 'pari'
     } else {
-        return 'odd'
+        return 'dispari'
     }
 }
+
+//Creo una costante vuota per determinare il risultato
+
+let risposta = ""
+
+if (risultato === numeroPariDispari) {
+    risposta = `Hai vinto, il tuo numero è ${risultato}: ${totale}`
+} else {
+    risposta = `Hai perso, il tuo numero è ${risultato}: ${totale}`
+}
+alert(risposta)
+
+
+
+
+
+
+
+
+
